05edf6f16319dae3732be240bac060a0a22406ed
LeonDevByu <leonardo.byu@gmail.com>
2023-01-21 16:40:47 -0500

    test

---
+++ python_week03/example_17.py
@@ -0,0 +1,23 @@
+# def kilometers_from_miles(miles):
+#     """Convert a value in miles to kilometers
+#     and return the kilometers value.
+#     """
+#     miles = float(input("Please enter a distance in miles: "))
+#     km = miles * 1.60934
+#     return km
+
+
+# import math
+# def torus_volume(inner, outer):
+#     """Compute and return the volume of a torus."""
+#     vol = 2 * (math.pi * inner) ** 2 * outer
+
+def compute(x, y, z):
+    result = compute(3, 2, x)
+
+    bmi = (x + y) / (z ** 2) * 10000
+    # return bmi
+
+    print(f"{bmi:.2f}")
+
+    return (1, 2, 3)
